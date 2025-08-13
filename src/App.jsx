1150b61b281aa7a2a659de5c64be0635ec79557e
Niraj Prajapati <154351209+Niraj-Hitpump@users.noreply.github.com>
2025-08-13 16:55:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import StatsPage from "./components/StatsPage";
 import RedirectPage from "./components/RedirectPage";
